a4e445c04066bf82e5304a19b6dcc063cad45830
Shartechs <shartechs@gmail.com>
2024-01-06 22:16:50 +0400

    added fake requests structure

---
+++ src/api/requests.js
@@ -0,0 +1,11 @@
+import data from "./data.json";
+
+export const getDestinations = () => {
+  return data.destinations;
+};
+export const getCrew = () => {
+  return data.crew;
+};
+export const getTechnology = () => {
+  return data.technology;
+};
