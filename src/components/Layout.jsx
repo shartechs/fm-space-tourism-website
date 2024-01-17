aafd84f900dfa5e7e827deacfea43de80b1d85ca
Shartechs <shartechs@gmail.com>
2024-01-17 19:57:39 +0400

    added responsive styles

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,7 +3,7 @@ import Navigation from "./Navigation";
 function Layout({ children, backgroundImageUrl }) {
   return (
     <div
-      className={`min-h-screen bg-cover bg-center bg-[url("src/assets/images/home/background-home-desktop.jpg")]`}
+      className={`min-h-screen md:pt-[40px] pl-[25px] md:pl-[40px] lg:pl-[55px] bg-cover bg-center bg-[url("src/assets/images/home/background-home-desktop.jpg")]`}
     >
       <Navigation />
       {children}
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -4,7 +4,7 @@ function Navigation() {
       <div>
         <img src="src/assets/svgs/logo.svg" alt="logo" />
       </div>
-      <ul className="flex gap-4 bg-opacity-[4%] backdrop-blur-[10px] shrink-0 justify-center content-center px-[50px] py-[25px]">
+      <ul className="flex gap-4 bg-opacity-[4%] backdrop-blur-[60px] shrink-0 justify-center content-center px-[50px] py-[25px] w-[60%]">
         <li className="navlink">Home</li>
         <li className="navlink">Destination</li>
         <li className="navlink">Crew</li>
