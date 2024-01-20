a72c6e6210c494517ec4f3c1eefab2f1a9de9e5b
Shartechs <shartechs@gmail.com>
2024-01-20 23:04:16 +0400

    added mobile navigation animation with framer

--- src/components/Navigation/MobileNavigation.jsx
+++ src/components/Navigation/MobileNavigation.jsx
@@ -1,4 +1,20 @@
-function MobileNavigation() {
-  return <div>MobileNavigation</div>;
+import { motion } from "framer-motion";
+
+const variants = {
+  "open": { x: 0, transition: { type: "tween", ease: "linear" } },
+  "closed": { x: 250, transition: { type: "tween", ease: "linear" } },
+};
+
+function MobileNavigation({ isOpen }) {
+  return (
+    <motion.div
+      variants={variants}
+      animate={isOpen ? "open" : "closed"}
+      initial="closed"
+      className="w-[250px] bg-opacity-[4%] backdrop-blur-[60px] h-[100vh] fixed right-0 top-0"
+    >
+      MobileNavigation
+    </motion.div>
+  );
 }
 export default MobileNavigation;
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,33 +1,44 @@
 import Navlink from "./NavLink";
 import useScreenSize from "../../hooks/useScreenSize";
+import MobileNavigation from "./MobileNavigation";
+import IconHamburger from "../../assets/svgs/IconHamburger";
+import { useState } from "react";
 
 function Navigation() {
   const screenSize = useScreenSize();
+  const [isOpen, setIsOpen] = useState(false);
   console.log(screenSize);
-  return screenSize.width > 768 ? (
-    <nav className="flex justify-between">
-      <div>
-        <Navlink to="/">
-          <img src="src/assets/svgs/logo.svg" alt="logo" />
-        </Navlink>
-      </div>
-      <ul className="flex gap-4 bg-opacity-[4%] backdrop-blur-[60px] shrink-0 justify-center content-center px-[50px] py-[25px] w-[60%]">
-        <li>
+  return (
+    <>
+      <nav className="flex justify-between">
+        <div>
           <Navlink to="/">
-            <span>01</span> Home
+            <img src="src/assets/svgs/logo.svg" alt="logo" />
           </Navlink>
-        </li>
-        <li>
-          <Navlink to="/destination">
-            <span>02</span> Destination
-          </Navlink>
-        </li>
-        <li className="navlink">Crew</li>
-        <li className="navlink">Technology</li>
-      </ul>
-    </nav>
-  ) : (
-    <div></div>
+        </div>
+        {screenSize.width > 768 ? (
+          <ul className="flex gap-4 bg-opacity-[4%] backdrop-blur-[60px] shrink-0 justify-center content-center px-[50px] py-[25px] w-[60%]">
+            <li>
+              <Navlink to="/">
+                <span>01</span> Home
+              </Navlink>
+            </li>
+            <li>
+              <Navlink to="/destination">
+                <span>02</span> Destination
+              </Navlink>
+            </li>
+            <li className="navlink">Crew</li>
+            <li className="navlink">Technology</li>
+          </ul>
+        ) : (
+          <button onClick={() => setIsOpen(true)}>
+            <IconHamburger />
+          </button>
+        )}
+      </nav>
+      <MobileNavigation isOpen={isOpen} />
+    </>
   );
 }
 export default Navigation;
