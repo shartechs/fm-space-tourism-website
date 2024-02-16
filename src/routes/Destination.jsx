a786b4f89388f3684222bde98986770709c74fce
Shartechs <shartechs@gmail.com>
2024-02-16 20:48:13 +0400

    destination initial markup

--- src/routes/Destination.jsx
+++ src/routes/Destination.jsx
@@ -1,3 +1,4 @@
+import { getDestinations } from "../api/requests";
 import Layout from "../components/Layout";
 const backgroundImages = {
   mobile: "src/assets/images/destination/background-destination-mobile.jpg",
@@ -6,6 +7,40 @@ const backgroundImages = {
 };
 
 const Destination = () => {
-  return <Layout backgroundImageUrl={backgroundImages}>Destination</Layout>;
+  const destinations = getDestinations();
+
+  return (
+    <Layout backgroundImageUrl={backgroundImages}>
+      <div className="flex flex-col mt-[100px]">
+        <h5 className="font-bellefair text-h5-desktop uppercase">
+          <span>01</span> PICK YOUR DESTINATION
+        </h5>
+        <div className="flex">
+          <div className="flex basis-6/12 justify-center items-center">
+            {" "}
+            <img src={destinations[1].images.png} alt="" />
+          </div>
+          <div className="basis-6/12">
+            <div className="flex gap-10">
+              {destinations.map((destination) => (
+                <button className="navlink pb-1" key={destination.name}>
+                  {destination.name}
+                </button>
+              ))}
+            </div>
+            <div>
+              <h1 className="font-bellefair text-h1-desktop uppercase">Mars</h1>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere
+                eligendi esse deleniti maiores cumque, natus dolores hic optio
+                sequi quos explicabo veniam! Ut incidunt impedit earum non
+                ducimus. Ab, molestiae.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
 };
 export default Destination;
